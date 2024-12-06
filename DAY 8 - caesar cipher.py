83d2736aedda7fcef8f50b0cc0b71163fd51077b
Oroku Jesse <jesse.oroku83@gmail.com>
2024-12-06 20:02:42 +0100

    add comment

---
+++ DAY 8 - caesar cipher.py
@@ -0,0 +1,52 @@
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount,direction):
+    if direction=='encode':
+        cipher_text = ""
+        for letter in original_text:
+            if letter not in alphabet:
+                cipher_text+=letter
+            else:
+                shifted_position = alphabet.index(letter) + shift_amount
+                shifted_position %= len(alphabet)
+                cipher_text += alphabet[shifted_position]
+        print(f"Here is the encoded result: {cipher_text}")
+    else:
+        cipher_text = ""
+        for letter in original_text:
+            if letter not in alphabet:
+                cipher_text+=letter
+            else:
+                shifted_position = alphabet.index(letter) - shift_amount
+                shifted_position %= len(alphabet)
+                cipher_text += alphabet[shifted_position]
+        print(f"Here is the decoded result: {cipher_text}")
+again=True
+while again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift,direction=direction)
+    restart=input('Do you plan to encode or decode any other message YES or NO\n').lower()
+    if restart=='no':
+        again=False
+        print('thank you for your time bro')
+
